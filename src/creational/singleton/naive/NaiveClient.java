1e0bead21ee2e56130c6f1e9c957c22b5ea7cf51
ycp <qkrdpcks0419@naver.com>
2018-03-10 05:47:26 +0900

    Singletone Pattern - 완료

--- src/creational/singleton/naive/NaiveClient.java
+++ src/creational/singleton/naive/NaiveClient.java
@@ -1,7 +1,13 @@
 package creational.singleton.naive;
 
 public class NaiveClient {
-    public static void main(String args[]) {
-        
+    private static final int THREAD_NUM = 5;
+
+    public static void main(String[] args) {
+        NaiveThread[] user = new NaiveThread[THREAD_NUM];
+        for (int i = 0; i < THREAD_NUM; i++) {
+            user[i] = new NaiveThread((i + 1) + "-thread");
+            user[i].start();
+        }
     }
 }
