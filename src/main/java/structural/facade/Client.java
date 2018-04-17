b6fc6a1efadce28604f355fe4d4503fbc2566afe
ycp <qkrdpcks0419@naver.com>
2018-04-17 21:18:14 +0900

    Facade Pattern - Client 소스코드 수정

--- src/main/java/structural/facade/Client.java
+++ src/main/java/structural/facade/Client.java
@@ -7,12 +7,15 @@ import structural.facade.facade.Facade;
 
 public class Client {
     public static void main(String args[]) {
-        Facade facade1 = new ConcreteFacade1();
-        Facade facade2 = new ConcreteFacade2();
-        Facade facade3 = new ConcreteFacade3();
+        Facade facade;
 
-        facade1.operation();
-        facade2.operation();
-        facade3.operation();
+        facade = new ConcreteFacade1();
+        facade.operation();
+
+        facade = new ConcreteFacade2();
+        facade.operation();
+
+        facade = new ConcreteFacade3();
+        facade.operation();
     }
 }
